33736b54b1c1b5bdbe1c6769fc3de84f1d1bd801
toshkobz <shinings878@gmail.com>
2026-04-03 14:59:50 +0300

    Practising loops and arrays. Exercise 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ... args) {
+return arr = arr.filter(item => !args.includes(item));
 };
+
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
