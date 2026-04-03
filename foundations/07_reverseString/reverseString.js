855daa2b396ebeb1bc358faf04b7466591fad8e8
toshkobz <shinings878@gmail.com>
2026-04-03 09:35:26 +0300

    Practising loops and arrays. Exercise 07_ReverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+ let result = str.split('').reverse().join('');
+
+ return result
 
 };
+
+console.log(reverseString('hello there'));
+
 
 // Do not edit below this line
 module.exports = reverseString;
