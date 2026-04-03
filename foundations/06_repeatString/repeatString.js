27271c6aac84915c1f99a4ffd5399f04d1a16d47
toshkobz <shinings878@gmail.com>
2026-04-03 09:24:01 +0300

    Practising loops, exercise 06_RepeatStrings

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
-};
+    let result = '';
+
+    if (num < 0) {
+        return `ERROR`
+    }
+
+    for (let i = 0; i < num; i++) {
+result += str;
+    }
+
+    return  result
+
+ }
+
+
+
+
+
+console.log(repeatString('hey', 3));
+
 
 // Do not edit below this line
 module.exports = repeatString;
